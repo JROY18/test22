cf77d0fa9a26107fba297a92668d8ab6c36fc58e
JROY18 <91453576+JROY18@users.noreply.github.com>
2024-07-15 14:45:00 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,22 @@
+public class add {
+    public static void main(String[] args){
+
+        int x = 1;
+        int y = 4;
+
+        
+        System.out.println(adt(x,y));
+
+    }
+
+    static int adt (int a,int b){
+
+        
+        return a+b;
+        
+
+    }
+    
+}
+
+
